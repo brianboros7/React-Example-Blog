5aeaf04821e794c9f19af7fc279939e4a6f7016c
Brian Boros <Bpower@tests-MBP.attlocal.net>
2020-03-12 19:10:15 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    <div id="blog-main p-10 bg-secondary m-4 border"> 
+      <header className="App-header p-4 m-4">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+    </div>
     </div>
   );
 }
